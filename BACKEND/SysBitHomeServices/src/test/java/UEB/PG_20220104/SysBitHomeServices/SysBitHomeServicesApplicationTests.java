ca9f401ace7328434ac6d51a46e4067dd6ded222
davidleonardo28 <dbarreral@unbosque.edu.co>
2022-08-25 18:48:34 -0500

    DB-ConfigInicial

---
+++ BACKEND/SysBitHomeServices/src/test/java/UEB/PG_20220104/SysBitHomeServices/SysBitHomeServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package UEB.PG_20220104.SysBitHomeServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysBitHomeServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
