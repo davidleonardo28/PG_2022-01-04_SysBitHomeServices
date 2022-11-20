ef5d9be106b4d2b098bd5f60e78645a39d225c0e
David Barrera <dbarreral@unbosque.edu.co>
2022-11-20 16:28:42 -0500

    inicial Status

--- FRONTEND/angular-SysBitHomeServices/src/app/app-routing.module.ts
+++ FRONTEND/angular-SysBitHomeServices/src/app/app-routing.module.ts
@@ -4,25 +4,41 @@ import { AuthGuard } from '@auth/guards/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
-  { path: 'about', loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule) },
-  {path: 'registro', loadChildren: () => import('./pages/auth/registro/registro-module').then(m => m.RegistroModule) },
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./pages/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'about',
+    loadChildren: () =>
+      import('./pages/about/about.module').then((m) => m.AboutModule),
+  },
+  {
+    path: 'registro',
+    loadChildren: () =>
+      import('./pages/auth/registro/registro-module').then(
+        (m) => m.RegistroModule
+      ),
+  },
 
   {
     path: 'sign-in',
-    loadChildren: () => import('./pages/auth/sign-in/sign-in.module').then(m => m.SignInModule),
-    canActivate: [AuthGuard]
+    loadChildren: () =>
+      import('./pages/auth/sign-in/sign-in.module').then((m) => m.SignInModule),
+    canActivate: [AuthGuard],
   },
   {
     path: 'sign-up',
-    loadChildren: () => import('./pages/auth/sign-up/sign-up.module').then(m => m.SignUpModule),
-    canActivate: [AuthGuard]
+    loadChildren: () =>
+      import('./pages/auth/sign-up/sign-up.module').then((m) => m.SignUpModule),
+    canActivate: [AuthGuard],
   },
   { path: '**', redirectTo: '/home', pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
